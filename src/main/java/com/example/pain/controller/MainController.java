6cde46c95ed3357036cb561a3baff0d05e06875e
NeoD1m <duffman7915492@gmail.com>
2021-09-08 00:03:53 +0300

    крутые похороны бабашки оли 2

--- src/main/java/com/example/pain/controller/MainController.java
+++ src/main/java/com/example/pain/controller/MainController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Map;
 
@@ -71,12 +72,19 @@ public class MainController {
         model.put("messages",messages);
         return "main";
     }
-    @PostMapping("/main")
+
+    @PostMapping("/accept")
     public String review(
+            @RequestParam Message message,
+            Map<String,Object> model){
+        System.out.println("AAAAAAAAAAAAAAAAAA");
 
-    ){
-        return "main";
+        messageRepo.delete(message);
+        message.setStatus(Status.ACCEPTED);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
+        return "/";
     }
-
 
 }
